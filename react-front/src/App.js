474801e543ddc5aa343c041e0d977fa2c954196e
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-12-08 15:52:38 +0500

    react-front-end

---
+++ react-front/src/App.js
@@ -0,0 +1,26 @@
+// recipe-front/src/App.js
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/yay")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  console.log(data);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
